801667bcad50524ae8dbd2e782cdc5a611626124
beliefs22-work <beliefs22@gmail.com>
2017-10-19 12:03:09 -0400

    Added gitignore file. Simplied code as I was repeating logic. Because threads and dir_to_save will be None if not given by user, I can use one function and test for Noneness in the convert_video function

--- find_and_convert_media_files.py
+++ find_and_convert_media_files.py
@@ -16,53 +16,42 @@ def main():
     # TODO: can you use a try except block with argparse exceptions?
     try:
         args = parser.parse_args(sys.argv[1:])
-        folder = args.vin
+        dir_to_search = args.vin
         file_type = args.ftype
         dir_to_save = args.vout
         threads = args.threads
         print_header()
-        folder = get_folder_from_user(folder)
+        dir_to_search = get_folder_from_user(dir_to_search)
         # TODO: see if you can add directory checks into argparser
-        if not folder:  # confirm you got a directory
-            print(folder)
+        if dir_to_search is None:  # confirm you got a directory
+            print(dir_to_search)
             print("Sorry we can't search that location.")
-            return
+            sys.exit(1)
         if dir_to_save:  # confirm you got a valid directory to save if you didn't get None from argparse
             dir_to_save = get_folder_from_user(dir_to_save)
-            if not dir_to_save:
+            if dir_to_save is None:
                 print("Sorry we can't save files there")
-                return
+                sys.exit(1)
 
         if not file_type.strip():  # confirm you got a valid file type and not an empty line
             print("You did not provide a file type!")
-            return
+            sys.exit(1)
 
-        print("Searching {} for {} type files".format(folder, file_type))
-        matches = search_folders(folder, file_type)
-        if dir_to_save:
-            for video in matches:
-                print(video)
-                video_name = os.path.basename(video)
-                input_folder = os.path.dirname(video)
-                start = time.time()
-                convert_video_to_mp4(video_name, file_type, input_folder, dir_to_save=dir_to_save, threads=threads)
-                end = time.time()
-                print("Converted in {} minutes".format((end - start) / 60))
-            else:
-                print("Sorry we can't search there")
-        else:
-            for video in matches:
-                print(video)
-                video_name = os.path.basename(video)
-                input_folder = os.path.dirname(video)
-                start = time.time()
-                convert_video_to_mp4(video_name, file_type, input_folder, threads=threads)
-                end = time.time()
-                print("Converted in {} minutes".format((end - start) / 60))
+        print("Searching {} for {} type files".format(dir_to_search, file_type))
+        matches = search_folders(dir_to_search, file_type)
+        for video in matches:
+            print(video)
+            video_name = os.path.basename(video)
+            dir_to_search = os.path.dirname(video)
+            start = time.time()
+            # Both directory and threads will be done if not given when function was called
+            convert_video_to_mp4(video_name, file_type, dir_to_search, dir_to_save=dir_to_save, threads=threads)
+            end = time.time()
+            print("Converted in {} minutes".format((end - start) / 60))
     except Exception as e:
         print("Sorry something went wrong")
         print(e)
-        return
+        sys.exit(1)
 
 
 def print_header():
@@ -126,7 +115,7 @@ def convert_video_to_mp4(old_video, file_type_to_convert, input_folder, dir_to_s
     :return:
     """
     old_video_path = os.path.join(input_folder, old_video)
-    if dir_to_save:
+    if dir_to_save is not None:
         new_video_path = os.path.join(dir_to_save, old_video.replace(file_type_to_convert, "mp4"))
     else:  # save in current directory
         new_video_path = old_video_path.replace(file_type_to_convert, "mp4")
@@ -134,7 +123,7 @@ def convert_video_to_mp4(old_video, file_type_to_convert, input_folder, dir_to_s
     if not os.path.isfile(old_video_path):
         print("{} is not a file".format(old_video_path))
         return
-    if threads:
+    if threads is not None:
         subprocess.run(['ffmpeg', '-i',
                         old_video_path, "-strict", "-2", "-threads", threads, new_video_path], stdin=subprocess.PIPE,
                        stderr=subprocess.PIPE, stdout=subprocess.PIPE)
@@ -145,54 +134,45 @@ def convert_video_to_mp4(old_video, file_type_to_convert, input_folder, dir_to_s
     print("Finished converting {} saved at {}".format(old_video, new_video_path))
 
 
-def find_and_convert_media_files(input_folder, file_type, output_folder=None, threads=None):
+def find_and_convert_media_files(dir_to_search, file_type, dir_to_save=None, threads=None):
     """
-    Locates media files or the given type in the given folder and converts them to mp4 format
+    Locates media files or the given type in the given dir_to_search and converts them to mp4 format
     :param threads: number of threads to limit ffmpeg to. If blank will use all threads (ffmpeg default)
-    :param input_folder: directory that contains the media files
+    :param dir_to_search: directory that contains the media files
     :param file_type: file type to search for i.e. avi
-    :param output_folder: directory to save files to. If not given, will save in files current directory
+    :param dir_to_save: directory to save files to. If not given, will save in files current directory
     :return:
     """
     # TODO: Add options for different file types this can serve as the skelton for each type
     print_header()
-    folder = input_folder
-    folder = get_folder_from_user(folder)
-    if not folder:
-        print(folder)
+    dir_to_search = dir_to_search
+    dir_to_search = get_folder_from_user(dir_to_search)
+
+    if dir_to_search is None:  # confirm you got a directory
+        print(dir_to_search)
         print("Sorry we can't search that location.")
-        return
-    file_type = file_type
-
-    if not file_type.strip():
-        print("We can't search for nothing!")
-        return
-
-    print("Searching {} for {} type files".format(folder, file_type))
-
-    matches = search_folders(folder, file_type)
-    if output_folder:
-        dir_to_save = output_folder
+        sys.exit(1)
+    if dir_to_save is not None:  # confirm you got a valid directory to save if you didn't get None from argparse
         dir_to_save = get_folder_from_user(dir_to_save)
-        if dir_to_save:
-            for video in matches:
-                video_name = os.path.basename(video)
-                input_folder = os.path.dirname(video)  # Find specific input folder since it searchers recursively
-                start = time.time()
-                convert_video_to_mp4(video_name, file_type, input_folder, dir_to_save=dir_to_save, threads=threads)
-                end = time.time()
-                print("Converted in {} minutes".format((end - start) / 60))
-        else:
-            print("Sorry we can't search there")
-    else:
-        for video in matches:
-            print(video)
-            video_name = os.path.basename(video)
-            input_folder = os.path.dirname(video)
-            start = time.time()
-            convert_video_to_mp4(video_name, file_type, input_folder, threads=threads)
-            end = time.time()
-            print("Converted in {} minutes".format((end - start) / 60))
+        if dir_to_save is None:
+            print("Sorry we can't save files there")
+            sys.exit(1)
+
+    if not file_type.strip():  # confirm you got a valid file type and not an empty line
+        print("You did not provide a file type!")
+        sys.exit(1)
+
+    print("Searching {} for {} type files".format(dir_to_search, file_type))
+    matches = search_folders(dir_to_search, file_type)
+    for video in matches:
+        print(video)
+        video_name = os.path.basename(video)
+        dir_to_search = os.path.dirname(video)
+        start = time.time()
+        # Both directory and threads will be done if not given when function was called
+        convert_video_to_mp4(video_name, file_type, dir_to_search, dir_to_save=dir_to_save, threads=threads)
+        end = time.time()
+        print("Converted in {} minutes".format((end - start) / 60))
 
 
 if __name__ == '__main__':
